19eabb736f583b9d3f2b8baa9183bd231949c90a
Ryan Dong <ryandong@Ryans-MacBook-Pro.local>
2022-12-03 15:06:19 -0800

    files

---
+++ README.md
@@ -0,0 +1,17 @@
+# Note Taker Starter Code
+
+# Created a Note Taker program
+
+# Made with HTML, CSS and JavaScript, Node, Express.JS, and Heroku
+
+# Creating Note Taker application that saves and retrieves notes from a JSON file.  
+
+# Learned how to write the of JavaScript, writing functions and using the Express.js framework to handle routing.
+
+# ![Portfolio Ryan Dong.](/challenge-11-note-taker-express/Develop/public/assets/localhost_3001_.png)
+
+# Ryan Dong
+
+# Ryandong7@gmail.com
+
+# Project Link: 
---
+++ server.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+
+const termData = require("./challenge-11-note-taker-express/Develop/db/db.json");
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => res.send("Navigate to /send or /routes"));
+
+app.get("/notes", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/notes.html"))
+);
+
+app.get("/paths", (req, res) =>
+  res.sendFile(path.join(__dirname, "public/paths.html"))
+);
+
+app.get("/api/notes", (req, res) => res.json(termData));
+app.post("/api/notes", (req, res) => {
+  var newTermData = termData;
+  newTermData.push(req.body);
+  // PROBLEM: req.body doesn't exist. so I have to push blah blah's
+  console.log(req.body);
+  // newTermData.push(req.body);
+
+  fs.writeFile("db/db.json", JSON.stringify(newTermData), (err) =>
+    err ? console.error(err) : console.log("Success!")
+  );
+
+  res.json(termData);
+  // res.json(newTermdata);
+});
+
+app.listen(PORT, () =>
+  console.log(`Example app listening at http://localhost:${PORT}`)
+);
